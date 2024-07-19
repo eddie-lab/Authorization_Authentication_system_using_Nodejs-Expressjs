a279e28143d4c7eb8a03dfbe467e1b1f4922f609
eddie-lab <eddiebob007@gmail.com>
2024-07-19 04:25:02 +0100

    Added Mailsender function in utils

--- utils/mailSender.js
+++ utils/mailSender.js
@@ -1,1 +1,30 @@
 const nodemailer  = require('nodemailer')
+
+const mailSender = async (email,title, body) =>{
+    try {
+        // Create a Transporter with SMTP details
+        let transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth: {
+                user : process.env.MAIL_USER, //SMTP user(email address)
+                pass : process.env.MAIL_PASS
+            }
+        })
+
+        // Send emails to users
+        let info = await transporter.sendMail({
+            from : 'MR-EDD <no-reply@eddiebob007@gmail.com>',
+            to: `${email}`,
+            subject :`title`,
+            html:`${body}`
+        })
+
+        console.log('Info is here:', info)
+        return info
+
+    }catch (error){
+        console.error(error.message)
+    }
+}
+
+module.exports = mailSender
