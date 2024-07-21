ca312f7b229cb2ed89f812d5e33c14da6afccbcc
eddie-lab <eddiebob007@gmail.com>
2024-07-21 20:31:06 +0100

    reconfigured database

--- config/database.js
+++ config/database.js
@@ -3,19 +3,17 @@ const mongoose = require('mongoose')
 require('dotenv').config()  //load enviroment variable from .env file
 
 
-exports.connect = () =>{
-    mongoose.connect(process.env.MONGODB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-        .then(() =>
-        console.log('connected to MongoDB'))
-        .catch((err) => {
-        console.log('failed to connect to mongoDB')
-        process.exit(1)
-    })
+exports.connect = async () =>{
+    try {
+        await mongoose.connect(process.env.MONGODB_URL, {
+            autoIndex: true       
+        })
+        console.log('Connected to Mongodb Atlas');
+
+    }catch (error){
+        console.error(error);
+    }
+
 }
-
-
 
 
